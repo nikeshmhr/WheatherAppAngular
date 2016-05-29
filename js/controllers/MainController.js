392ca403ac663f79de474f328ddd3cf9a6073502
Nikesh Maharjan <nikeshmhr@gmail.com>
2016-05-29 17:19:51 +0545

    Minor error fixed

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,6 +1,6 @@
 app.controller('MainController', function($scope){
 	$scope.API_KEY = "e7f585b242780035570c4f35ae46f954";
-	$scope.BASE_URI = "http://api.openweathermap.org/data/2.5/weather?appid=" + API_KEY;
+	$scope.BASE_URI = "http://api.openweathermap.org/data/2.5/weather?appid=" + $scope.API_KEY;
 
 	$scope.constructRequestURI = function(city){
 		return $scope.BASE_URI + "&q=" + city.trim();
